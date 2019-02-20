941c17509a94c19ecd923507a353dcf44c5c6fc1
Shane4666 <sc897@nau.edu>
2019-02-19 18:09:22 -0700

    added buttons!!

--- src/Button.js
+++ src/Button.js
@@ -1,15 +1,18 @@
 // This page is based on: https://www.youtube.com/watch?v=LAaf7-WuJJQ Wes Bos is amazing!
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Button extends Component {
-
   onClick = () => {
     this.props.onClick();
-  }
+  };
 
   render() {
-    return (<button type="button" onClick={() => this.onClick()}>{this.props.label}</button>);  
+    return (
+      <button type="button" onClick={() => this.onClick()}>
+        {this.props.label}
+      </button>
+    );
   }
 }
 
--- src/Timer-Page.js
+++ src/Timer-Page.js
@@ -7,22 +7,35 @@ class TimerPage extends Component {
     super(props);
     this.state = {
       timerControlText: "Start",
-      timerPlaying: false,
-      timeLeft: 1200
+      timeLeft: 1200,
+      timerPlaying: false
     };
   }
 
   componentDidMount() {
-    this.timer(this.state.timeLeft);
+    this.setTime(this.state.timeLeft);
   }
 
   playButtonClick = () => {
-    this.setState({
-      ...this.state,
-      timerControlText:
-        this.state.timerControlText === "Start" ? "Pause" : "Start",
-      timerPlaying: this.state.timerControlText === "Start" ? false : true
-    });
+    this.setState(
+      {
+        ...this.state,
+        timerControlText:
+          this.state.timerControlText === "Start" ? "Pause" : "Start",
+        timerPlaying: !this.state.timerPlaying
+      },
+      () => this.timer(this.state.timeLeft)
+    );
+  };
+
+  setTimeLeft = seconds => {
+    this.setState(
+      {
+        ...this.state,
+        timeLeft: seconds
+      },
+      () => this.setTime(this.state.timeLeft)
+    );
   };
 
   setTime = seconds => {
@@ -35,24 +48,35 @@ class TimerPage extends Component {
   timer = seconds => {
     const now = Date.now();
     const then = now + seconds * 1000;
-    this.setTime(seconds);
-
-    const countDown = setInterval(() => {
-      const secondsLeft = Math.round((then - Date.now()) / 1000);
-      if (secondsLeft <= 0) {
-        clearInterval(countDown);
-        return;
-      }
-      this.setTime(secondsLeft);
-    }, 1000);
+    let countDown;
+    if (this.state.timerPlaying) {
+      countDown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft <= 0) {
+          clearInterval(this.state.countDown);
+          return;
+        }
+        this.setTime(secondsLeft);
+      }, 1000);
+      this.setState({
+        ...this.state,
+        countDown
+      });
+    } else {
+      clearInterval(this.state.countDown);
+    }
   };
 
   render() {
     return (
-      <div class="timer-page">
+      <div className="timer-page">
         <Timer seconds={this.state.timeLeft} />
+        <Button onClick={() => this.setTimeLeft(300)} label={5} />
+        <Button onClick={() => this.setTimeLeft(600)} label={10} />
+        <Button onClick={() => this.setTimeLeft(900)} label={15} />
+        <Button onClick={() => this.setTimeLeft(1200)} label={20} />
         <Button
-          onClick={this.playButtonClick.bind(this)}
+          onClick={this.playButtonClick}
           label={this.state.timerControlText}
         />
       </div>
