a571174784206840aaeeaed1e286dfb098839d3c
Shane4666 <sc897@nau.edu>
2019-02-21 12:21:14 -0700

    comment for future update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* Fix github config to get Router working with github pages */}
         <Router>
           <TimerPage path="/" />
         </Router>
