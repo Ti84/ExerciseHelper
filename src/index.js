ea3b11d90624451545f2d735eb322a197d5e65d8
Shane4666 <sc897@nau.edu>
2019-02-22 21:11:28 -0700

    moved serviceworker path fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./component/App";
-import * as serviceWorker from "./component/serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
