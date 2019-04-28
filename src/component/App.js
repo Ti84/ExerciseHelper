6121a74c95857fca4bf00b1a32cbe90eb7a37558
Shane Cook <sc897@nau.edu>
2019-04-27 23:07:10 -0700

    to do list

--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,18 @@ import React, { Component } from "react";
 import "./App.scss";
 import TimerPage from "./TimerPage";
 
+// ToDo:
+// 1. Add Row Feature
+// 2. Edit Rows Feature
+// 3. Drop down for Months
+// 4. Style Habits Page
+// 5. Redux Implemented
+// 6. Server Side (Login / Calendar)
+// 7. Login Page
+// 8. CLIENT SIDE - Activity Breakdowns (Stats on selected activity)
+// 9. SERVER SIDE - Activity Breakdowns (Stats on selected activity)
+// 10. Accessibility
+
 class App extends Component {
   render() {
     return (
